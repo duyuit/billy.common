435ad95a2b023ed81b3cbe7ba61990679cbee313
Duy Nguyen <duykkxm97@gmail.com>
2023-03-28 23:01:35 +0700

    Init

---
+++ Runtime/MyCube.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace billy.common
+{
+    public class MyCube : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.LogError("Hello world, my name is Cube");
+        }
+
+    }
+
+}
